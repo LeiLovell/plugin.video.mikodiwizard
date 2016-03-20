15494d9c157868a710e6648880b294ddd475edc4
LeiLovell <leigh_lovell@hotmail.com>
2016-03-20 17:32:16 +0000

    Update default.py

--- default.py
+++ default.py
@@ -20,7 +20,7 @@ PATH = "mikodiwizard"
 
 
 def CATEGORIES():
-    link = OPEN_URL('http://androidtv.ie/tomswizard.txt').replace('\n','').replace('\r','')
+    link = OPEN_URL('http://mikodi.uk/miswiz.txt').replace('\n','').replace('\r','')
     match = re.compile('name="(.+?)".+?rl="(.+?)".+?mg="(.+?)".+?anart="(.+?)".+?escription="(.+?)"').findall(link)
     for name,url,iconimage,fanart,description in match:
         addDir(name,url,1,iconimage,fanart,description)
